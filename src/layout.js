b64189f2ca2dcdd169611c36215f0ba0f451f9bb
Pat Cavit <github@patcavit.com>
2017-05-23 17:00:47 -0700

    break out nav component

--- src/layout.js
+++ src/layout.js
@@ -1,11 +1,8 @@
 import m from "mithril";
 
 import pkg from "../package.json";
-import meta from "../data/meta.json";
-import state from "./state.js";
 
-import Icon from "./icon.js";
-import Totals from "./totals.js";
+import Nav from "./nav.js";
 
 import css from "./layout.css";
 
@@ -17,30 +14,7 @@ export default function() {
                     m("span", { class : css.subtitle }, `v${pkg.version}`)
                 ),
 
-                m("div", { class : css.choices },
-                    m("div", { class : css.parts },
-                        meta.order.map((type) =>
-                            m("a", {
-                                    href     : `/${type}`,
-                                    oncreate : m.route.link,
-                                    class    : css.part
-                                },
-                                state[type] ?
-                                    m(Icon, {
-                                        type,
-                                        key  : state[type],
-                                        icon : state[type]
-                                    }) :
-                                    m(Icon, {
-                                        type : "unselected",
-                                        key  : type,
-                                        icon : type
-                                    })
-                            )
-                        )
-                    ),
-                    m(Totals)
-                ),
+                m(Nav),
 
                 vnode.children
             ];
---
+++ src/nav.js
@@ -0,0 +1,40 @@
+import m from "mithril";
+
+import meta from "../data/meta.json";
+import state from "./state.js";
+
+import Icon from "./icon.js";
+import Totals from "./totals.js";
+
+import css from "./nav.css";
+
+export default function() {
+    return {
+        view() {
+            return m("div", { class : css.choices },
+                m("div", { class : css.parts },
+                    meta.order.map((type) =>
+                        m("a", {
+                                href     : `/${type}`,
+                                oncreate : m.route.link,
+                                class    : css.part
+                            },
+                            state[type] ?
+                                m(Icon, {
+                                    type,
+                                    key  : state[type],
+                                    icon : state[type]
+                                }) :
+                                m(Icon, {
+                                    type : "unselected",
+                                    key  : type,
+                                    icon : type
+                                })
+                        )
+                    )
+                ),
+                m(Totals)
+            );
+        }
+    }
+}
