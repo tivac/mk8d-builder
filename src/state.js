62729ab5484608331f2dc4e248baec9c3cb016eb
Pat Cavit <github@patcavit.com>
2017-05-24 00:00:53 -0700

    feat: prep for saving state
    No mechanisms in place yet though

--- src/state.js
+++ src/state.js
@@ -1,6 +1,24 @@
-export default {
+var state = {
     character : null,
     kart      : null,
     tire      : null,
     glider    : null
 };
+
+export default state;
+
+export function encode() {
+    return atob(JSON.stringify(state));
+}
+
+export function decode() {
+    var kart = location.hash;
+
+    console.log(kart);
+
+    try {
+        state = JSON.parse(atob(kart));
+    } catch(e) {
+        // no-op for now
+    }
+}
